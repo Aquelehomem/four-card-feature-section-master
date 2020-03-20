16cce29a385262bd9fab26a20c6a239fe10dcc92
Aquelehomem <gustavomartinsdp@outlook.com>
2020-03-20 18:07:44 -0300

    Updates

---
+++ precache-manifest.e2c27faa22b014cfd286ed4fca61c619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e619460313f6e00d51b44707fdedf21e",
+    "url": "/four-card-feature-section-master/index.html"
+  },
+  {
+    "revision": "869193fc76bb32264818",
+    "url": "/four-card-feature-section-master/static/css/main.cce5f892.chunk.css"
+  },
+  {
+    "revision": "b7b465a114c753f18e2b",
+    "url": "/four-card-feature-section-master/static/js/2.0c53f370.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/four-card-feature-section-master/static/js/2.0c53f370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "869193fc76bb32264818",
+    "url": "/four-card-feature-section-master/static/js/main.845fd82f.chunk.js"
+  },
+  {
+    "revision": "88bc8a519bbac4e71e9a",
+    "url": "/four-card-feature-section-master/static/js/runtime-main.7f7bfe32.js"
+  }
+]);
